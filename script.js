10518f0242efa75ab2e102350cf38fe596999599
TheAjazAli <ajazali78890@gmail.com>
2025-04-28 16:57:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 AOS.init({
                     duration: 1000,  // animation duration in ms
                     once: true       // animate only once when scrolling
+                    startEvent: 'load'
                 });
